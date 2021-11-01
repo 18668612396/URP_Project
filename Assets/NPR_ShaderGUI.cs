18232ff1a86fc8d27ab774f03603b89eb22d150d
qiaoziyang <715174381@qq.com>
2021-11-02 03:22:21 +0800

    ds

--- Assets/NPR_ShaderGUI.cs
+++ Assets/NPR_ShaderGUI.cs
@@ -6,23 +6,29 @@ public class NPR_ShaderGUI : ShaderGUI
 
     //定义材质属性
     Material material;
-    MaterialProperty _ShaderEnumProp;
-    //基础PBR材质参数
-    MaterialProperty _MainTexProp;
 
     public override void OnGUI(MaterialEditor materialEditor, MaterialProperty[] properties)
     {
-        _ShaderEnumProp = FindProperty("_ShaderEnum", properties);
+
         //获取当前材质球
         material = materialEditor.target as Material;
+
         ShaderGUI(materialEditor, properties);
         Other(materialEditor);//其他额外参数绘制
     }
 
     //绘制入口
-
+    //NPR公共材质参数
+    MaterialProperty _ShaderEnumProp;
+    MaterialProperty _MainTexProp;
+    MaterialProperty _ParamTexProp;
     private void ShaderGUI(MaterialEditor materialEditor, MaterialProperty[] properties)
     {
+        //NPR公共参数
+        _ShaderEnumProp = FindProperty("_ShaderEnum", properties);
+        _MainTexProp = FindProperty("_MainTex", properties);
+        _ParamTexProp = FindProperty("_ParamTex", properties);
+
         materialEditor.ShaderProperty(_ShaderEnumProp, "材质类型");
         EditorGUILayout.BeginHorizontal(new GUIStyle("horizontalslider"));//绘制分割线
         EditorGUILayout.EndVertical();
@@ -30,27 +36,91 @@ public class NPR_ShaderGUI : ShaderGUI
 
         if (material.IsKeywordEnabled("_SHADERENUM_BODY"))
         {
-            BaseParam(materialEditor, properties);//传递参数
+            BodyParam(materialEditor, properties);//传递参数
         }
         if (material.IsKeywordEnabled("_SHADERENUM_FACE"))
         {
             EditorGUILayout.IntField(0);
         }
-        if (material.IsKeywordEnabled("_SHADERENUM_HARI"))
+        if (material.IsKeywordEnabled("_SHADERENUM_HAIR"))
         {
             EditorGUILayout.IntField(1);
         }
     }
-
-    //将定义好的材质属性传入Shader
-    private void BaseParam(MaterialEditor materialEditor, MaterialProperty[] properties)
+    //身体部分GUI
+    bool _SkinToggle;
+    MaterialProperty _SkinFactorProp;
+    MaterialProperty _SkinFactorToleranceProp;
+    //丝绸
+    bool _SilkToggle;
+    MaterialProperty _SilkFactorProp;
+    MaterialProperty _SilkFactorToleranceProp;
+    private void BodyParam(MaterialEditor materialEditor, MaterialProperty[] properties)
     {
-        //PBR材质参数
-        _MainTexProp = FindProperty("_MainTex", properties);
         //颜色贴图  且判断其是否被赋值
         Texture _MainTex = material.GetTexture("_MainTex");
         materialEditor.TexturePropertySingleLine(new GUIContent("颜色贴图"), _MainTexProp);//绘制主颜色纹理GUI
+        materialEditor.TexturePropertySingleLine(new GUIContent("参数贴图"), _ParamTexProp);
+        //皮肤部分
+        _SkinFactorProp = FindProperty("_SkinFactor", properties);
+        _SkinFactorToleranceProp = FindProperty("_SkinFactorTolerance", properties);
+
+        _SkinToggle = material.IsKeywordEnabled("_SKINTOGGLE_ON") ? true : false; //读取Keywords数据
+        _SkinToggle = EditorGUILayout.BeginToggleGroup(new GUIContent("SKIN_TOGGLE"), _SkinToggle);
+        if (_SkinToggle)
+        {
+
+            material.EnableKeyword("_SKINTOGGLE_ON");
+            materialEditor.RangeProperty(_SkinFactorProp, "皮肤遮罩阈值");
+            materialEditor.RangeProperty(_SkinFactorToleranceProp, "遮罩容差");
+        }
+        else
+        {
+
+            material.DisableKeyword("_SKINTOGGLE_ON");
+        }
+        EditorGUILayout.EndToggleGroup();
+
+        //丝绸部分
+        _SilkFactorProp = FindProperty("_SilkFactor", properties);
+        _SilkFactorToleranceProp = FindProperty("_SilkFactorTolerance", properties);
+
+        _SilkToggle = material.IsKeywordEnabled("_SILKTOGGLE_ON") ? true : false; //读取Keywords数据
+        _SilkToggle = EditorGUILayout.BeginToggleGroup(new GUIContent("SILK_TOGGLE"), _SilkToggle);
+        if (_SilkToggle)
+        {
+
+            material.EnableKeyword("_SILKTOGGLE_ON");
+            materialEditor.RangeProperty(_SilkFactorProp, "丝绸遮罩阈值");
+            materialEditor.RangeProperty(_SilkFactorToleranceProp, "遮罩容差");
+        }
+        else
+        {
+
+            material.DisableKeyword("_SILKTOGGLE_ON");
+        }
+        EditorGUILayout.EndToggleGroup();
+
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     private void Other(MaterialEditor materialEditor)
     {
         EditorGUILayout.Space(10);
--- Packages/com.custom-universal.shader/Shader/Character/NPR_Shader.shader
+++ Packages/com.custom-universal.shader/Shader/Character/NPR_Shader.shader
@@ -4,12 +4,19 @@ Shader "Custom/Character/CartoonShader"
 {
     Properties
     {
-        [KeywordEnum(Body,Face,Hari)] _ShaderEnum("ShaderEnum",int) = 0
+        //基础
+        [KeywordEnum(Body,Face,Hair)] _ShaderEnum("ShaderEnum",int) = 0
         _MainTex ("MainTex", 2D) = "white" {}
         _ParamTex("_ParamTex",2D) = "white"{}
         
-        [Header(Skin)]
-        _SilkFactor("_SilkFactor",int) = 0
+        //皮肤
+        [Toggle]_SkinToggle("SkinToggle",int) = 0
+        _SkinFactor("SkinFactor",Range(0.0,255.0)) = 0
+        _SkinFactorTolerance("SkinFactorTolerance",Range(0.0,50.0)) = 20.0
+        //丝绸
+        [Toggle]_SilkToggle("SilkToggle",int) = 0
+        _SilkFactor("SilkFactor",Range(0.0,255.0)) = 0
+        _SilkFactorTolerance("SilkFactorTolerance",Range(0.0,50.0)) = 20.0
         
     }
     SubShader
@@ -31,7 +38,10 @@ Shader "Custom/Character/CartoonShader"
         #pragma shader_feature _WINDANIM_ON _WINDANIM_OFF
         #pragma shader_feature _CLOUDSHADOW_ON _CLOUDSHADOW_OFF
         #pragma shader_feature _WORLDFOG_ON _WORLDFOG_OFF
-        #pragma shader_feature _SHADERENUM_BODY _SHADERENUM_FACE _SHADERENUM_HARI
+        #pragma shader_feature _SHADERENUM_BODY _SHADERENUM_FACE _SHADERENUM_HAIR
+
+        #pragma shader_feature _SKINTOGGLE_ON 
+        #pragma shader_feature _SILKTOGGLE_ON 
         uniform TEXTURE2D (_MainTex);
         uniform	SAMPLER(sampler_MainTex);
 
@@ -39,9 +49,7 @@ Shader "Custom/Character/CartoonShader"
         uniform SAMPLER(sampler_ParamTex);
         //变量声明
         CBUFFER_START(UnityPerMaterial)
-        //贴图采样器
-        uniform float4 _MainTex_ST;
-        int _SilkFactor;
+
         CBUFFER_END
         //结构体
         #pragma vertex vert
@@ -51,7 +59,8 @@ Shader "Custom/Character/CartoonShader"
         #pragma multi_compile _ _SHADOWS_SOFT
         struct NPR
         {
-            float3 baseColor;
+            float3 baseMap;
+            float4 paramMap;
         };
 
 
@@ -88,7 +97,7 @@ Shader "Custom/Character/CartoonShader"
             {
                 v2f o;
                 ZERO_INITIALIZE(v2f,o);//初始化顶点着色器
-                o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+                o.uv = v.uv;
                 o.pos = TransformObjectToHClip(v.vertex.xyz);
                 o.worldPos = TransformObjectToWorld(v.vertex.xyz);//这个一定要放在下面两个顶点相关的方法之上 这样他们才能调用到
                 o.worldNormal = TransformObjectToWorldNormal(v.normal);
@@ -103,30 +112,16 @@ Shader "Custom/Character/CartoonShader"
                 //采样贴图
                 float4 var_MainTex = SAMPLE_TEXTURE2D(_MainTex,sampler_MainTex,i.uv);
                 float4 var_ParamTex = SAMPLE_TEXTURE2D(_ParamTex,sampler_ParamTex,i.uv);
-                float maskFactor = var_ParamTex.a*255;
-                //丝绸
-                // float silkFactor = maskFactor > _SilkFactor + 20||maskFactor < _SilkFactor - 20 ? 0.0:1.0;
 
-                float silkFactor = min(step(maskFactor,_SilkFactor + 20),step(maskFactor,_SilkFactor - 20));
 
                 NPR npr;
-                // ZERO_INITIALIZE(NPR,npr);//初始化顶点着色器
-
-
-
-
-
+                ZERO_INITIALIZE(NPR,npr);//初始化顶点着色器
+                npr.baseMap = var_MainTex;
+                npr.paramMap = var_ParamTex;
 
                 // NPR_FUNCTION(i,npr);
-
-                #if _SHADERENUM_BODY
-                return 0.0;
-                #elif _SHADERENUM_FACE
-                return 0.5;
-                #elif _SHADERENUM_HARI
-                return 1.0;
-                #endif
-
+                float3 finalRGB = NPR_FUNCTION(i,npr);
+                return finalRGB;
             }
             ENDHLSL
         }
@@ -153,5 +148,5 @@ Shader "Custom/Character/CartoonShader"
         
     }
 
-  CustomEditor "NPR_ShaderGUI"  
+    CustomEditor "NPR_ShaderGUI"  
 }
